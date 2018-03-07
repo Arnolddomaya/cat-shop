ba6cfaeae223ea92d2f163122ccd465b033f6072
arnold-domaya <domaya_arnold@yahoo.fr>
2018-03-07 12:20:17 +0100

    bd ok

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,36 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
+  def index
+    @items = Item.all
+  end
+
+  def show
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def item_params
+      params.require(:item).permit(
+         :title,
+         :description,
+         :price,
+         :image_url)
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.create( email:"dodo@gmail.com", password:"motdepasse", password_confirmation:"motdepasse")
+User.create( email:"dada@gmail.com", password:"motdepasse", password_confirmation:"motdepasse")
+
+my_items =[]
+
+i=1
+10.times do
+  my_items << {  title:"MonChat#{i}",
+  description:"Description de MonChat#{i}",
+  price: i,
+  image_url: "cat_img_#{i}"}
+  i+=1
+end
+
+cats = Item.create(my_items)
